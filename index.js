9d991eced73cfef9a3c5bdb62f142435a397d996
nilotpal <nil@nil.nil>
2026-04-16 20:53:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
