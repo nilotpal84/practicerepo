9eb8f2688f79d6a0eefb760a5dce6f98797b21b4
nilotpal84 <nilotpal97488@gmail.com>
2026-04-18 01:41:41 +0530

    Update API endpoint in test to new IP address

--- server.test.js
+++ server.test.js
@@ -7,7 +7,7 @@ async function fetchData() {
 test('fetches data from API', async () => {
   const data = 5;
   //axios.get.mockResolvedValue({ data: 2, status: 200 });
-  const response = await axios.get('http://localhost:3000');
+  const response = await axios.get('http://192.168.0.101:3000');
   const result = response.data;
   expect(response.status).toBe(200);
   expect(result).toEqual(2);
